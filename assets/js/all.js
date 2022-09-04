907110405515874c6b5801a5e9309e845dd79478
Manson <manson972@yahoo.com>
2022-09-05 01:22:01 +0800

    Update 2022-09-04T17:21:58.968Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,24 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var programSwiper = new Swiper(".programSwiper", {
+  loop: true,
+  slidesPerView: "auto",
+  loopedSlides: 17,
+  grabCursor: true,
+  mousewheel: true,
+  freeMode: true,
+  speed: 1500,
+  autoplay: {
+    delay: 0,
+    waitForTransition: false
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 8
+    },
+    768: {
+      spaceBetween: 16
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
