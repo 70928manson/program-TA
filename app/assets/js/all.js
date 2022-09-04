51efc0802569d57b5b95f680b16ba2b766cd4d5f
Manson <manson972@yahoo.com>
2022-09-05 01:21:41 +0800

    update feature process program course block

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,21 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+let programSwiper = new Swiper(".programSwiper", {
+  loop: true,
+  slidesPerView: "auto",
+  loopedSlides: 17,
+  grabCursor: true,
+  mousewheel: true,
+  freeMode: true,
+  speed: 1500,
+  autoplay: {
+    delay: 0,
+    waitForTransition: false
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 8
+    },
+    768: {
+      spaceBetween: 16
+    }
+  }
 });
